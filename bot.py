8120d6502db85daaceb0e0d12d9bb5d5db71a1b2
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2021-03-02 23:08:23 +0530

    Limit the reactions to user json response only

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 
 load_dotenv('.env')
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-print(CLIENT_ID)
-print(CLIENT_SECRET)
-print(ACCESS_TOKEN)
+
 url = "https://api.linkedin.com/v2/organizationAcls?q=organization&organization=urn%3Ali%3Aorganization%3A14547467&role=ADMINISTRATOR&state=APPROVED"
 
-admin_request_url = "https://api.linkedin.com/v2/reactions/(entity:urn%3Ali%3Aactivity%3A6750434325692596224)?q=entity"
+admin_request_url = "https://api.linkedin.com/v2/reactions/(entity:urn%3Ali%3Aactivity%3A6750434325692596224)?q=entity&count=67&projection=(elements)"
 headers={
     "Connection": "Keep-Alive",
     "Authorization": "Bearer {}".format(ACCESS_TOKEN),
@@ -30,4 +27,18 @@ print(re.json())
 
 re = requests.get(admin_request_url, headers=headers_admin)
 print(re.status_code)
-print(re.json())
+json_text = json.loads(re.text)
+#print(json_text)
+elements = json_text["elements"]
+
+for element in elements:
+    actor = element["created"]["actor"]
+    actor_elements = actor.split(':')
+    actor_id = actor_elements[-1]
+
+    url = "https://api.linkedin.com/v2/people/(id:{})".format(actor_id)
+    print(url)
+    re = requests.get(url, headers=headers_admin)
+    print(re.status_code)
+    print(re.text)
+
