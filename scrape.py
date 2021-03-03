2fb57fd3b4b9c166e511036fe7bdd2a2c469b524
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2021-03-03 19:37:29 +0530

    Fix the total likes counter

--- scrape.py
+++ scrape.py
@@ -8,5 +8,7 @@ src = r.text
 print(src)
 soup = BeautifulSoup(src, features="lxml")
 likes = soup.find_all("span", {"class": "social-counts-reactions__social-counts-numRections"})
+likes_details = soup.find_all("div", {"class": "artdeco-modal artdeco-modal--layer-default social-details-reactors-modal"})
 
 print(likes)
+print(likes_details)
